2266b9b9ed3c8f264b596b5a7691dc05d92b1200
Jordan Gordeev <jg@example.com>
2024-11-22 11:02:31 +0200

    Initial Commit

---
+++ Exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exercise
+//
+//  Created by Jordan Gordeev on 22.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
